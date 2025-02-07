0bf5dd16425e4e500b312c089aace96932664791
GarimaChhabra201186 <garimabatra83@gmail.com>
2025-02-07 16:30:30 +0530

    added ML model code

---
+++ Model.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import statsmodel.api as sm
+
+print("Addind all ML related codes")
